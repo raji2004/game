47caa54d873b9ae60534f2d9e49fac659d9b8877
raji2004 <kingraj1344@gmail.com>
2022-10-14 20:14:32 +0100

    there is a bug

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 let body = document.body;
-let h2 = document.querySelector('h2');
-let input = document.getElementById('input-field');
-let ul = document.getElementById('items-to-shop');
-let listItems = document.querySelectorAll('li');
+const h2 = document.querySelector("h2");
+const input = document.getElementById("input-field");
+const ul = document.getElementById("items-to-shop");
+const listItems = document.querySelectorAll("li");
+const err = document.getElementById("error");
+const lis = ul.children;
+let c = 0;
+let button = document.getElementById("btn");
+button.addEventListener("click", list);
 
-let button = document.getElementById('btn');
-button.addEventListener('click', () => {
-    if (input.value !== ""){
-        let found = false;
-        listItems.forEach(listitem => {
-            if (listitem.innerText === input.value){
-                found = true;
-                listitem.innerText = `${listitem.innerText} x2`
-                input.value = '';
-            }
-        });
-        if (found){
-            // alert('Item already exists')
-        }
-        else{
-            let newListItem = document.createElement('li');
-            let listContent = document.createTextNode(input.value);
+// adding an event listner to each li\
+for (let p = 0; p < lis.length; p++) {
+  lis[p].addEventListener("click", counter);
+}
 
-            // body.append(ul);
-            ul.append(newListItem);
-            newListItem.append(listContent);
-            input.value = '';
-        } 
+function list(e) {
+  console.log(ul.children);
+  if (input.value !== "") {
+    for (let i = 0; i < ul.children.length; i++) {
+      console.log(ul.children[i].innerText);
+      if (ul.children[i].innerText === input.value) {
+        err.innerText =
+          "Item already exists click on the item to increase its quantity";
+        input.value = "";
+        return;
+      }
     }
-});
-
-
-
-
-
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    ul.appendChild(li);
+    input.value = "";
+  }
+}
+function counter() {
+  c++;
+  this.innerText = `${this.innerText} X${c}`;
+}
 
 // write a condition
 /* if the input value is already present in the list, return nothing
